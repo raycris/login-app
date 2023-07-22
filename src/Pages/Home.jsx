82dea8cc747d88ee0cb3782d296abd622f5cd28c
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-07-22 19:42:52 -0400

    app ready to go

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const getUser = () => {
+  let user = localStorage.getItem("user");
+  if (user) {
+    user = JSON.parse(user);
+  } else {
+    user = null;
+  }
+  return user;
+};
+
 const Home = () => {
+  const [user, setUser] = useState(getUser());
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
   return (
     <>
-      <Link
-        to="/login"
-        className="h-12 w-28 bg-zinc-800 text-white rounded-lg flex justify-center items-center"
-      >
-        LOGIN
-      </Link>
+      {user ? (
+        <>
+          <h4>
+            Hello, {user.firstName} {user.lastName}
+          </h4>
+          <h5>{user.email}</h5>
+          <button
+            className="bg-orange-500 w-28 h-14 rounded-lg flex justify-center items-center"
+            onClick={handleLogout}
+          >
+            LOGOUT
+          </button>
+        </>
+      ) : (
+        <Link
+          to="/login"
+          className="h-12 w-28 bg-zinc-800 text-white rounded-lg flex justify-center items-center"
+        >
+          LOGIN
+        </Link>
+      )}
     </>
   );
 };
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../Store/userSlice";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const { loading, error } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLoginEvent = (e) => {
+    e.preventDefault();
+    let userCredentials = {
+      email,
+      password,
+    };
+    dispatch(loginUser(userCredentials)).then((result) => {
+      if (result.payload) {
+        setEmail("");
+        setpassword("");
+        navigate("/");
+      }
+    });
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form>
+        <form onSubmit={handleLoginEvent}>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -12,11 +40,13 @@ const Login = () => {
               Username
             </label>
             <input
+              required
               type="email"
               id="username"
               name="username"
               className="w-full border border-gray-300 rounded p-2 focus:outline-none focus:border-blue-400"
-              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -27,19 +57,24 @@ const Login = () => {
               Password
             </label>
             <input
+              required
               type="password"
               id="password"
               name="password"
               className="w-full border border-gray-300 rounded p-2 focus:outline-none focus:border-blue-400"
-              required
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
             />
           </div>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
-            Login
+            {loading ? "Loading..." : "Login"}
           </button>
+          {error&&(
+            <div className="bg-red-500" role="alert">{error}</div>
+          )}
         </form>
       </div>
     </div>
